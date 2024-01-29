51f63e6959383dd695886a3ca6aefa245d04d086
Davann Tet <davanntet@gmail.com>
2024-01-29 18:18:07 +0700

    time_spend

--- time_spend.sql
+++ time_spend.sql
@@ -1,6 +1,8 @@
 use mdl;
 select _user.id as user_id,
-sum(distinct _sessions.timemodified-_sessions.timecreated)/(60*60) as 'duration(hours)'
+sum(distinct _sessions.timemodified-_sessions.timecreated)/(60*60) as 'duration(hours)',
+min(_sessions.timecreated) as 'first',
+max(_sessions.timemodified) as 'last'
 from mdl_user as _user
 inner join mdl_role_assignments as _user_role on _user_role.userid = _user.id and _user_role.roleid=5
 left join mdl_sessions as _sessions on _sessions.userid=_user.id
