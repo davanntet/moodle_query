da2df463d6cda2fef0d419c14c2c4b40b3455ef9
Davann Tet <davanntet@gmail.com>
2024-01-29 18:22:47 +0700

    teacher_course

--- course_teacher.sql
+++ course_teacher.sql
@@ -1,5 +1,5 @@
 use mdl;
-select _user.id as user_id,_user.username as teacher,_course.id as course_id,_course.fullname as course
+select _user.id as user_id,_user.username as teacher_username,_user.firstname,_user.lastname,_course.id as course_id,_course.fullname as course
 from mdl_user as _user
 inner join mdl_role_assignments as _user_role on _user_role.userid = _user.id and _user_role.roleid=4
 left join mdl_user_enrolments as _uenrol on _user.id = _uenrol.userid
